4943f04067346650e52b03808e1cfb1d6eb89b90
Arnav Patel <patelarnav96@gmail.com>
2017-07-24 10:07:53 +0530

    test print statement

--- randomnumbergen2.py
+++ randomnumbergen2.py
@@ -23,6 +23,7 @@ def randomword():
    poatemplate = open('template.txt','r')
 
    poatext = poatemplate.read()
+   print poatext
    poatext.decode('utf-8')
    poatext.replace('[GRANTORNAME]',name)
    poatext.replace('[GRANTORADDRESS]',address)
