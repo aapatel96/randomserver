567ba633292d75c6846b6e4587c0a745c003a4fd
Arnav Patel <patelarnav96@gmail.com>
2017-07-24 18:58:43 +0530

    add name check and mongo integration

--- randomnumbergen.py
+++ randomnumbergen.py
@@ -7,14 +7,27 @@ from flask import Flask, request, jsonify
 
 ##bot = telegram.Bot('395089971:AAGdmNnerGxByQZqhjen2hAGIZ2CBW-WcnY')
 
-
+client = pymongo.MongoClient(os.environ['MONGODB_URI'])
+db = client.get_default_database()
+active = db['active']
 
 app = Flask(__name__)
 
 @app.route("/random", methods=['POST'])
 def randomword():
+   name = request.form['name']
    try:
-      return jsonify(status='success',string=''.join(random.choice(string.lowercase) for i in range(int(request.form['length']))))
+   	  string = ''.join(random.choice(string.lowercase) for i in range(int(request.form['length'])))
+   	  isInDB = True
+   	  while isInDB == True:
+   	  	stringInDBactive.find_one({'string':string,'name':name})
+   	  	if stringInDB == None:
+   	  		active.insert_one({'string':string,'name':name})
+   	  		break
+   	  	else:
+   	  		string = ''.join(random.choice(string.lowercase) for i in range(int(request.form['length'])))
+
+      return jsonify(status='success',string=string,name=name)
    except:
       return jsonify(status='failed',error="Please pass numbers only")
 
