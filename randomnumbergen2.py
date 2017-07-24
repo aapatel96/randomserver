0847029c2d92a6a6233cc8d9f73863393a0485d8
Arnav Patel <patelarnav96@gmail.com>
2017-07-24 11:17:32 +0530

    remove str()

--- randomnumbergen2.py
+++ randomnumbergen2.py
@@ -28,9 +28,9 @@ def randomword():
    print type(address)
    print type(passport_number)
 
-   poatext.replace('GRANTORNAME',str(name))
-   poatext.replace('GRANTORADDRESS',str(address))
-   poatext.replace('GRANTORPASSPORTNUMBER',str(passport_number))
+   poatext.replace('GRANTORNAME',name)
+   poatext.replace('GRANTORADDRESS',address)
+   poatext.replace('GRANTORPASSPORTNUMBER',passport_number)
 
    userfilename = name+'-poa.txt'
    userfile = open(userfilename,'w')
